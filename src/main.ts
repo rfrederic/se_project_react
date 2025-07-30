2ed1d3b9233c2739fbb1fe7ecb135cf2077382e5
RigaudsonFrederic <frigaudson1997@gmail.com>
2025-07-30 05:58:20 -0400

    New update

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import './index.css';
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
