2ef56c434469576ab567988492c21dbfe8fed97e
RigaudsonFrederic <frigaudson1997@gmail.com>
2025-07-30 19:15:44 -0400

    set up fonts.css file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
-import "./components/App/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
