f022c30e33bc41ce41033937ffa25aaf8504107a
RigaudsonFrederic <frigaudson1997@gmail.com>
2025-07-30 08:48:54 -0400

    New

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./components/App/index.css";
 import App from "./components/App/App";
-import "./index.css";
+import "./components/App/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
